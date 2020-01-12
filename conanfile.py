c01bebf92f59ce352625974c3e7cbe3da57eea06
Kevin <kevin@kevinkarsch.com>
2020-01-11 20:49:41 -0800

    Upgrade openssl and use lowercase (for other repo compatibility)

--- conanfile.py
+++ conanfile.py
@@ -45,7 +45,7 @@ of Things (IoT)"""
 
     def requirements(self):
         if self.options.SSL:
-            self.requires("OpenSSL/1.0.2s@conan/stable")
+            self.requires("openssl/1.0.2t@conan/stable")
 
     def _configure_cmake(self):
         cmake = CMake(self)
